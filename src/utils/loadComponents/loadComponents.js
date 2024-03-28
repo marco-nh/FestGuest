64f65c73f5ca695b06f243440b45c4108ce3ca30
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2024-03-28 21:19:06 +0000

    add load component

---
+++ src/utils/loadComponents/loadComponents.js
@@ -0,0 +1,10 @@
+function loadComponents(fileName, id, callback) {
+    fetch(fileName).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById(id).innerHTML = text;
+        if (callback) {
+            callback();
+        }
+    })
+}
