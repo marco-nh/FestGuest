8079e0c4a4b83a873a73c14dc8cb3d53965e9458
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2024-04-16 10:17:20 +0100

    problem with view transition  delete temp

--- src/utils/viewTransition/viewTransition.js
+++ src/utils/viewTransition/viewTransition.js
@@ -1,3 +1,6 @@
+/** BUG DETECTED!!!! DONT TOUCH
+ *  PROBLEM WITH DOM TRANSITION
+ *  
 if (document.startViewTransition) {
     window.navigation.addEventListener('navigate', async (event) => {
         const toUrl = new URL(event.destination.url);
@@ -5,23 +8,29 @@ if (document.startViewTransition) {
         // Ignorar si es una página externa
         if (location.origin !== toUrl.origin) return;
 
-        // Interceptación de la navegación en el mismo dominio
         event.intercept({
             async handler() {
                 try {
-                    // Cargar la página de destino utilizando fetch para obtener el HTML
-                    const response = await fetch(toUrl.pathname);
+                    // Cargar la página de destino completa
+                    const response = await fetch(toUrl.href);
                     if (!response.ok) {
                         throw new Error('Failed to load page');
                     }
-                    // Extraer el contenido de la página de destino
-                    const text = await response.text();
-                    const tempElement = document.createElement('div');
-                    tempElement.innerHTML = text;
-                    const newContent = tempElement.querySelector('#content').innerHTML;
-                    // Actualizar la página con el nuevo contenido
+                    // Extraer el HTML de la respuesta
+                    const html = await response.text();
+                    
+                    // Verificar si el elemento body existe en el HTML recibido
+                    const bodyElement = document.createElement('div');
+                    bodyElement.innerHTML = html;
+                    const newContent = bodyElement.querySelector('body') ? bodyElement.querySelector('body').innerHTML : html;
+
+                    // Aplicar la transición de vista después de cargar la nueva página
                     document.startViewTransition(() => {
+                        // Actualizar el contenido de la página con el nuevo contenido
                         document.getElementById('content').innerHTML = newContent;
+                        // Llamar a la función para cargar scripts y estilos
+                        loadScripts();
+                        // Desplazar al inicio de la página
                         document.documentElement.scrollTop = 0;
                     });
                 } catch (error) {
@@ -31,3 +40,4 @@ if (document.startViewTransition) {
         });
     });
 }
+*/
