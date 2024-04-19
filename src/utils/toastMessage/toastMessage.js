7b4cf2646c58205481ca20d37fe3a2f0be826504
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2024-04-20 00:36:50 +0100

    add toast function

---
+++ src/utils/toastMessage/toastMessage.js
@@ -0,0 +1,18 @@
+// Función para mostrar mensajes
+export function showMessage(message, type = "success") {
+    Toastify({
+      text: message,
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "bottom",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: type === "success" ? "light-blue" : "orange",
+        fontWeight: "bold"
+      },
+      onClick: function () {}
+    }).showToast();
+  }
+  
