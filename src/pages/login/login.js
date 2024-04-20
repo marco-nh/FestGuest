5ffbc0d4b6007b3c89f37d2d224deddd1d903fcd
AlejandroDavidArzolaSaavedra <alejandro.arzola101@alu.ulpgc.es>
2024-04-20 05:39:16 +0100

    unexpeceted var use let in login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,8 +4,8 @@ import { showMessage } from "../../utils/toastMessage/toastMessage.js";
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('login-form').addEventListener('submit', async (e) => {
       e.preventDefault(); 
-      var email = document.getElementById('emaillog').value;
-      var password = document.getElementById('passwordlog').value;
+      let email = document.getElementById('emaillog').value;
+      let password = document.getElementById('passwordlog').value;
       try {
         await signInWithEmail(email, password);
         window.location.href = "/src/index.html";
