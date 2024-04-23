ba5d47c9080f1571dbbebce2729ee89b26f5ae4a
AlejandroArzolaSaavedraDavid <alejandro.arzola101@alu.ulpgc.es>
2024-04-23 23:35:07 +0100

    refactor category images

---
+++ src/utils/category/category.js
@@ -0,0 +1,20 @@
+export const categoryImages = {
+    'school-holidays': 5,
+    'public-holidays': 5,
+    'observances': 1,
+    'politics': 1,
+    'conferences': 1,
+    'expos': 1,
+    'concerts': 1,
+    'festivals': 1,
+    'performing-arts': 1,
+    'sports': 1,
+    'community': 1,
+    'daylight-savings': 1,
+    'airport-delays': 1,
+    'severe-weather': 1,
+    'disasters': 1,
+    'terror': 1,
+    'health-warnings': 1,
+    'academic': 1
+};
