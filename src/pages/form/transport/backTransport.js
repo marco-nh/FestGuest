c6f3cab8b377a09128d850108f955ffc299c926f
AlejandroArzolaSaavedraDavid <alejandro.arzola101@alu.ulpgc.es>
2024-04-30 08:38:32 +0100

    change fetch to await

--- src/pages/form/transport/backTransport.js
+++ src/pages/form/transport/backTransport.js
@@ -24,9 +24,9 @@ function agregarSugerencias(inputId, suggestionsId) {
   });
 }
 
-function fetchSuggestions(query) {
-  return fetch(`https://api.tomtom.com/search/2/search/${encodeURIComponent(query)}.json?key=BsJ3AWQpgcXR8jiUM4E6AAYYgjNopDLy&limit=3&countrySet=ES`)
-    .then(response => response.json());
+async function fetchSuggestions(query) {
+  const response = await fetch(`https://api.tomtom.com/search/2/search/${encodeURIComponent(query)}.json?key=BsJ3AWQpgcXR8jiUM4E6AAYYgjNopDLy&limit=3&countrySet=ES`);
+  return await response.json();
 }
 
 function displaySuggestions(data, suggestionsPanel, input) {
