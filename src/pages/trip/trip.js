c47da99c0ce5c0d55cc2790caa8ab52686017531
marco-nh <47418876+marco-nh@users.noreply.github.com>
2024-05-03 22:42:02 +0100

    Relaciones Casas Festivales y Añadido suscripciones a eventos (+UI mejorado)

--- src/pages/trip/trip.js
+++ src/pages/trip/trip.js
@@ -1,10 +1,29 @@
 //firebase
 import { app } from "../../firebase/initializeDatabase.js";
-import { getDocs, getFirestore, collection} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import { doc, setDoc,query,where, getDocs, getFirestore, collection} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
-function initiateRender(){
 
+function initializeOthers(titulo) {
+    const suscripcionButon = document.getElementById("suscripcionButon")
+    const desuscripcionButon = document.getElementById("disabledsuscripcionButon")
+    const arrayReservados = JSON.parse(localStorage.getItem("eventosSuscritos"))
+    try {
+        arrayReservados.forEach((fiesta) => {
+            if (fiesta == titulo){
+                suscripcionButon.classList.add("hidden")
+                console.log("Reservado")
+                throw BreakException
+            }
+        })
+    } catch (e) {
+        console.log("Fallo: Existe suscripcion")
+        return false
+    };
+
+    desuscripcionButon.classList.add("hidden")
 }
+
+
 async function getTransport(container,title){
     const db = getFirestore(app);
     const querySnapshot = await getDocs(collection(db, "transports"));
@@ -19,8 +38,35 @@ async function getTransport(container,title){
             }
         })
 }
+async function getAccomodations(container,title){
+    const db = getFirestore(app);
+    const querySnapshot = await getDocs(collection(db, "accommodations"));
+    let checkExists = false
+    querySnapshot.forEach((doc) => {
+            checkExists = doc.data().festivalAsociado == title;
+            if (checkExists){
+                const card = createTransportCard(doc.data())
+                container.appendChild(card)
+            }
+        })
+}
+
+function renderAccomodations(location){
+    const divTransp = document.getElementById("contentAccomodation")
+    divTransp.classList.remove("hidden")
+    const base = document.getElementById("baseAccomodation")
+
+    const container = document.createElement('div');
+    container.classList.add('grid', 'gap-4');
+
+    getAccomodations(container,location)
+
+    base.appendChild(container);
+}
 
 function renderTransport(location){
+    const divTransp = document.getElementById("transportDiv")
+    divTransp.classList.remove("hidden")
     const base = document.getElementById("base")
 
     const container = document.createElement('div');
@@ -85,6 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const storedEvent = JSON.parse(localStorage.getItem('selectedEvent'));
     const infoEvent = storedEvent.event;
     localStorage.setItem("transportRendered","false")
+    localStorage.setItem("accomodationRendered","false")
     if (storedEvent) {
         const eventNameElement = document.getElementById('eventName');
         const eventImageElement = document.getElementById('eventImage');
@@ -126,9 +173,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const accommodationsButton = document.getElementById('accommodationsButton');
         accommodationsButton.addEventListener('click', function() {
+            const transportContent = document.getElementById('contentAccomodation')
+            transportContent.classList.remove('hidden')
             // Pendiente implementar Buscar las acomodaciones relacionados con el evento(Revisar local storage)
+            if (localStorage.getItem("accomodationRendered") != "true"){
+                renderAccomodations(infoEvent.title)
+                localStorage.setItem("accomodationRendered","true")
+                console.log(localStorage.getItem("accomodationRendered"))
+            }
         });
     }
+    initializeOthers(infoEvent.title);
 
     
         
@@ -142,9 +197,83 @@ document.getElementById('chatBoton').addEventListener('click', function(){
     const nombre = infoEvent.title;
     createChat(nombre);
     window.location.href = `/src/pages/chat/chat.html?chatName=${nombre}`
+    console.log(title)
 });
 
+//suscribirse
 
+document.getElementById("suscripcionButon").addEventListener('click', function(){
+    addSuscription()
+    console.log("Suscrito")
+});
+
+document.getElementById("disabledsuscripcionButon").addEventListener('click', function(){
+    removeSuscription()
+    console.log("Desuscrito")
+});
+
+async function addSuscription(){
+    const db = getFirestore(app);
+    const userDocString = localStorage.getItem('userDoc');
+    const suscripcionButon = document.getElementById("suscripcionButon")
+    const desuscripcion = document.getElementById("disabledsuscripcionButon")
+    if (userDocString) {
+        const userDoc = JSON.parse(userDocString);          
+        const querySnapshot = await getDocs(query(collection(db, 'users'), where('userEmail', '==', userDoc.userEmail)));
+        
+        querySnapshot.forEach((userdata) => {
+            const arraySuscritos = []
+            const fiesta = document.getElementById('eventName').textContent
+
+            if (userdata.data().festivalAsociado != null){
+                const arrayFiestas = userdata.data().festivalAsociado;
+
+                arrayFiestas.forEach((fiestaBase) => {
+                    arraySuscritos.push(fiestaBase)
+                    if (fiesta == fiestaBase){
+                        console.log("Ya ha sido reservado")
+                        localStorage.setItem("reservado","true")
+                        return false
+                    }
+                })
+                arraySuscritos.push(fiesta)
+            } else{
+                arraySuscritos.push(userdata.data().festivalAsociado)
+            }
+            
+            const userRef = doc(db, "users", userdata.id);
+            if (localStorage.getItem("reservado") == "false"){
+                setDoc(userRef, { festivalAsociado: arraySuscritos }, { merge: true });
+            }
+            //para evitar otra busqueda, agrego la id del usuario y lo meto en local
+            localStorage.setItem("userId",userdata.id)
+
+            localStorage.setItem("eventosSuscritos",JSON.stringify(arraySuscritos))
+            console.log("Guardado en local")
+
+            localStorage.setItem("reservado",false)
+        })
+        suscripcionButon.classList.add("hidden")
+        desuscripcion.classList.remove("hidden")
+    }
+}
+
+function removeSuscription() {
+    const db = getFirestore(app);
+    const fiesta = document.getElementById('eventName').textContent
+    const desuscripcion = document.getElementById("disabledsuscripcionButon")
+    const suscripcionButon = document.getElementById("suscripcionButon")
+
+    let arrayReservados = JSON.parse(localStorage.getItem("eventosSuscritos"))
+    arrayReservados = arrayReservados.filter(e => e !== fiesta); // will return ['A', 'C']
+    localStorage.setItem("eventosSuscritos",JSON.stringify(arrayReservados))
+    const userRef = doc(db, "users", localStorage.getItem("userId"));
+    setDoc(userRef, { festivalAsociado: arrayReservados }, { merge: true });
+
+    desuscripcion.classList.add("hidden")
+    suscripcionButon.classList.remove("hidden")
+
+}
 
 async function getLocationFromAPI(event) {
     let locationString = '';
