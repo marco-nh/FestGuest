c907e58ddace076afb87994b047e5892ca928419
user-nizan <114477663+user-nizan@users.noreply.github.com>
2024-05-05 12:34:38 +0100

    Interfaz de perfil

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -11,3 +11,40 @@ document.addEventListener("DOMContentLoaded", () => {
             });
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var correols = localStorage.getItem('userEmail');
+
+    // Obtener el elemento div por su ID
+    var correo = document.getElementById('correo');
+    
+    // Verificar si el valor de localStorage no es null o undefined
+    if (correols !== null && correols !== undefined) {
+        // Establecer el valor de localStorage como el contenido del div
+        correo.textContent = correols;
+    } else {
+        // Si no hay valor en localStorage, mostrar un mensaje de error o establecer un valor predeterminado
+        correo.textContent = 'No hay correo asociado a esta cuenta';
+    }    
+})
+    
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var userDocString = localStorage.getItem('userDoc');
+    var userDocObject = JSON.parse(userDocString);
+    var userName = userDocObject.userName;
+    // Obtener el elemento div por su ID
+    var nombre = document.getElementById('nombre');
+    
+    // Verificar si el valor de localStorage no es null o undefined
+    if (userName !== null && userName !== undefined) {
+        // Establecer el valor de localStorage como el contenido del div
+        nombre.textContent = userName;
+    } else {
+        // Si no hay valor en localStorage, mostrar un mensaje de error o establecer un valor predeterminado
+        nombre.textContent = 'No hay nombre asociado a esta cuenta';
+    }
+})
+
